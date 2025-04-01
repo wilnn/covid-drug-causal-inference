f1f52ce4e71e99401c3c78bcbde5be8c369d56fd
wilnn <thangnguyen15700@gmail.com>
2025-03-31 21:02:52 -0400

    add excel file

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -1,2 +1,8 @@
 import pandas as pd
+import numpy as np
+data = pd.read_csv("./data/N3C_data_10000_sample.csv")  # Skip header
+
+
+data.to_excel("./data/N3C_data_10000_sample.xlsx", index=False)
+
 
